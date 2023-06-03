9abe760fe1253bddfa9023b6a1a3f6500fe1c1b7
alotab <alotab@yahoo.com>
2023-06-03 04:55:47 +0000

    update user admin

--- users/admin.py
+++ users/admin.py
@@ -4,32 +4,7 @@ from django.contrib.auth.models import Group
 from .forms import CustomeUserChangeForm, UserCreationForm, CustomUserCreationsForm
 from .models import CustomUser
 
-# this will also change the admin interface 
-# class CustomUserAdmin(UserAdmin):   
-#     form = CustomeUserChangeForm
-#     add_form = CustomUserCreationsForm
-   
-#     model = CustomUser
-#     list_display = ["email", "first_name", "last_name","is_staff", "is_active"]
-#     list_filter = ["email", "is_staff", "is_active"]
-#     fieldsets = (
-#         (None, {"fields": ("email", "password", 'first_name', 'last_name', 'profile_image')}),
-#         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
-#     )
-#     add_fieldsets = (
-#         (None, {
-#             "classes": ("wide",),
-#             "fields": (
-#                 "email", "password1", "password2", "is_staff",
-#                 "is_active", "groups", "user_permissions"
-#             )}
-#         ),
-#     )
-#     search_fields = ("email",)
-#     ordering = ("email", "first_name",)
 
-
-# admin.site.register(CustomUser, CustomUserAdmin)
 
 class CustomUserAdmin(UserAdmin):
     add_form = UserCreationForm
