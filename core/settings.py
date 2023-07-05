b62c5953b7f3e1feddf0a3f9d66df5e6aa4840a8
alotab <alotab@yahoo.com>
2023-07-05 19:52:46 +0000

    update files

--- core/settings.py
+++ core/settings.py
@@ -156,8 +156,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
-    # BASE_DIR / "static",
-    # "static/css/styles.css",
+ 
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
